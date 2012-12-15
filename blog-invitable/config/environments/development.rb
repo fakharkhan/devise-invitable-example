0513e99c02560e45e3cdbdec8c5fdf85c743b601
Javid Jamae <javidjamae@gmail.com>
2012-12-14 21:20:45 -0600

    got the invitation / email working

--- blog-invitable/config/environments/development.rb
+++ blog-invitable/config/environments/development.rb
@@ -36,4 +36,17 @@ Blog::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD']
+  }
+  
 end
